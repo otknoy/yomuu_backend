1ba3aefd90282bce243051f3f9e7e6b6e7260e25
otknoy <otknoy@gmail.com>
2016-02-13 13:51:54 +0900

    Enable cors

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import json
-from bottle import route, run, request
+from bottle import hook, route, run, request
 
 from yomuu import preprocess
 from yomuu import preprocess
+
+@hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
+@route('/')
+def index():
+    print('hello')
 
 @route('/textFeature', 'GET')
 def get_text_feature():
